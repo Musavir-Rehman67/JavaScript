e797abb9a5af2895f7aa48024f4f6fc0cdf392fd
MUSAVIR REHMAN <87717099+Musavir-Rehman67@users.noreply.github.com>
2023-01-20 13:36:06 +0000

    Comparsion

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+
+//These types of conversion will put into confusion anytime(avoid)
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === check also datatype
+
+console.log("2" === 2);
+
